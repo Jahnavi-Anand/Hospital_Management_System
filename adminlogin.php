f223d4d308c64e0ef939b03eb25a387716092e0e
Swarnima <bishtswarnima@gmail.com>
2023-11-06 02:08:11 +0530

    new file:   adminlogin.php

---
+++ adminlogin.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Login Page</title>
+</head>
+<body>
+
+    <!-- PHP Admin login 03 Stamp- 1:24 -->
+    <div class="container">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-6">
+                    <form method="post">
+                        <div class="form-group">
+                            <label>Username</label>
+                            <input type="text" name="uname" class="form-control" autocomplete="off" placeholder="Enter Username">
+                        </div>
+                        <div>
+                            
+                        </div>
+
+                    </form>
+                </div>
+                <div class="col-md-3"></div>    
+            </div>
+        </div>
+    </div>
+</body>
+</html>
