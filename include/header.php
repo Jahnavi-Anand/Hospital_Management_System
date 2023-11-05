e6365a1a7aee4bbdb999401616e160ebc1f5a6a0
Swarnima <bishtswarnima@gmail.com>
2023-11-06 00:47:27 +0530

    modified:   include/header.php 	renamed:    include/index.php -> index.php

--- include/header.php
+++ include/header.php
@@ -1,29 +1,35 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title><title>
+    <title></title> <!-- Corrected the title tag -->
+    
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
     <script src="https://code.jquery.com/jquery-3.7.1.slim.js" integrity="sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc=" crossorigin="anonymous"></script>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-
+    
+    
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css">
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-info bg-info">
-
+    <div class="d-flex justify-content-between align-items-center w-100">
         <h5 class="text-white">Hospital Management System</h5>
-
+        
         <div class="mr-auto"></div>
 
-        <ul class="">
-            <li class="nav-item"><a href="" class="nav-link">Admin</a></li>
-            <li class="nav-item"><a href="" class="nav-link">Doctor</a></li>
-            <li class="nav-item"><a href="" class="nav-link">Patient</a></li>
-        </ul>
+            <ul class="navbar-nav"> 
+                <li class="nav-item"><a href="#" class="nav-link text-white">Admin</a></li> 
+                <li class="nav-item"><a href="#" class="nav-link text-white">Doctor</a></li>
+                <li class="nav-item"><a href="#" class="nav-link text-white">Patient</a></li>
 
+        
+            </ul>
+   
+        
+    
     </nav>
 
 </body>
