cef6c87c91a5edd4d6be642389a5aea0ce44d6de
AtrayeePathak <143244755+AtrayeePathak@users.noreply.github.com>
2023-11-07 00:08:32 +0530

    Update header.php

--- include/header.php
+++ include/header.php
@@ -1,39 +1,31 @@
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
-    <title></title> <!-- Corrected the title tag -->
+    <title></title>
     
+    <!-- Include Bootstrap CSS -->
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    
+    <!-- Include jQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-
-    <script src="https://code.jquery.com/jquery-3.7.1.slim.js" integrity="sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc=" crossorigin="anonymous"></script>
-
     
-    
-    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css">
-    <script type="text/javascript" src"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/brands.min.js"></script>
-    <link rel="stylesheet" type="text/css"  href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css">">
+    <!-- Include Font Awesome CSS and JavaScript -->
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
 </head>
-
 <body>
     <nav class="navbar navbar-expand-lg navbar-info bg-info">
-    <div class="d-flex justify-content-between align-items-center w-100">
-        <h5 class="text-white">Hospital Management System</h5>
-        
-        <div class="mr-auto"></div>
+        <div class="d-flex justify-content-between align-items-center w-100">
+            <h5 class="text-white">Hospital Management System</h5>
+            
+            <div class="mr-auto"></div>
 
             <ul class="navbar-nav"> 
                 <li class="nav-item"><a href="#" class="nav-link text-white">Admin</a></li> 
                 <li class="nav-item"><a href="#" class="nav-link text-white">Doctor</a></li>
                 <li class="nav-item"><a href="#" class="nav-link text-white">Patient</a></li>
-                <!-- PHP-Admin Login 03 33:43 -->
-        
             </ul>
-   
-        
-    
+        </div>
     </nav>
-
 </body>
-
 </html>
