fd5f091fbe1aa712a7d696a11135be7470420b35
kavya-singh101 <madhujeet111@gmail.com>
2023-11-07 00:49:52 +0530

    adding path for admin button

--- include/header.php
+++ include/header.php
@@ -3,10 +3,8 @@
 <head>
     <title></title>
     
-    <!-- Include Bootstrap CSS -->
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     
-    <!-- Include jQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     
     <!-- Include Font Awesome CSS and JavaScript -->
@@ -21,7 +19,7 @@
             <div class="mr-auto"></div>
 
             <ul class="navbar-nav"> 
-                <li class="nav-item"><a href="#" class="nav-link text-white">Admin</a></li> 
+                <li class="nav-item"><a href="adminlogin.php" class="nav-link text-white">Admin</a></li> 
                 <li class="nav-item"><a href="#" class="nav-link text-white">Doctor</a></li>
                 <li class="nav-item"><a href="#" class="nav-link text-white">Patient</a></li>
             </ul>
