2cb581344eb45c9b293d9e3deedf2b6c1949b3b4
Swarnima <bishtswarnima@gmail.com>
2023-11-06 21:12:18 +0530

    Create profile.php

---
+++ admin/profile.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Profile</title>
+</head>
+
+<body>
+    <?php
+    // Admin Profile Page 07- 0:57
+    ?>
+
+    <div class="container-fluid">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-2">
+                    <?php
+                        // Admin Profile 07 - 3:18
+                    ?>
+                </div>
+                <div class="col-md-10">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h4>Admin Profile</h4>
+
+                            <img src="">
+                        </div>
+                        <div class="col-md-6">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+
+</html>
