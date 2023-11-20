b3e0120fcdac58766926a5c3ccf32032e4b105ee
Parthhiv <parthhivashley@gmail.com>
2023-11-20 16:58:37 +0530

    Add invoice.php

---
+++ patient/invoice.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Invoice</title>
+</head>
+<body>
+    <?php
+        include("../include/header.php");
+        include("../include/connection.php");
+    ?>
+
+    <div class="container-fluid">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-2" style="margin-left: -30px">
+                <?php
+                    include("sidenav.php");
+                ?>
+                </div>
+                
+                <div class="col-md-10">
+                    <h5 class="text-center my-2">My Invoice</h5>
+
+                    <?php
+                        //vid17
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
