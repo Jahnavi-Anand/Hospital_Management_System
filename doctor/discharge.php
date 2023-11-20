e551c7918444d32ac7be278db75def4de2668767
Parthhiv <parthhivashley@gmail.com>
2023-11-20 16:43:51 +0530

    Add details table

--- doctor/discharge.php
+++ doctor/discharge.php
@@ -26,6 +26,58 @@
                 <!-- vid17 39:25 check line 105 to 108 in file index.php -->
                 <div class="col-md-10">
                     <h5 class="text-center my-2">Total Appointment</h5>
+
+                    <?php
+                        //check below php vid17 44:40
+
+                        if(isset($_GET['id'])){
+                            $id = $_GET['id'];
+                            $query = "SELECT * FROM appointment WHERE id='$id'";
+                            $res = mysqli_query($connect,$query);
+                            $row = mysqli_fetch_array($res);
+                        }
+
+                    ?>
+
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <table class="table table-bordered">
+                                    <tr>
+                                        <td colspan="2" class="text-center">Appointment Details</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Firstname</td>
+                                        <td><?php echo $row['firstname']; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Surname</td>
+                                        <td><?php echo $row['surname']; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Gender</td>
+                                        <td><?php echo $row['gender']; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Phone Number</td>
+                                        <td><?php echo $row['phone']; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Appointment Date</td>
+                                        <td><?php echo $row['appointment_date']; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Symptoms</td>
+                                        <td><?php echo $row['symptoms']; ?></td>
+                                    </tr>
+
+                                </table>
+                            </div>
+                            <div class="col-md-6">
+                                <h5 class="text-center my-1">Invoice</h5>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
