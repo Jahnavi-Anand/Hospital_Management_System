cc7b172bf7f517a4a729aa0cad19cd6f9d836e9e
Parthhiv <parthhivashley@gmail.com>
2023-11-20 16:20:00 +0530

    Add discharge.php

--- doctor/discharge.php
+++ doctor/discharge.php
@@ -1,17 +1,18 @@
 <?php
     session_start();
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Total Appointment</title>
+    <title>Check Patient Appointment</title>
 </head>
 <body>
     <?php
         include("../include/header.php");
-        include("../include/header.php");
+        include("../include/connection.php");
     ?>
 
     <div class="container-fluid">
@@ -19,18 +20,11 @@
             <div class="row">
                 <div class="col-md-2" style="margin-left: -30px;">
                     <?php
-                    include("sidenav.php");
-                    ?>
-                </div>
-                <div class="col-md-10">
-                    <h5 class="text-center my-2">Total Appointment</h5>
-                    <?php
-                        //vid17 26:12
+                        include("sidenav.php");
                     ?>
                 </div>
             </div>
         </div>
     </div>
-    
 </body>
 </html>
