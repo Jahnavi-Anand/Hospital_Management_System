60f7d4eaabf2e2d36006f4cb2155fb22fb70a16d
Parthhiv <parthhivashley@gmail.com>
2023-11-20 17:04:14 +0530

    Add header and sidenav bar

--- patient/view.php
+++ patient/view.php
@@ -9,6 +9,23 @@
     <title>View Invoice</title>
 </head>
 <body>
+
+    <?php
+        include("../include/header.php");
+        include("../include/connection.php");
+    ?>
+
+    <div class="container-fluid">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-2" style="margin-left: -30px">
+                    <?php
+                        include("sidenav.php");
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
     
 </body>
 </html>
