023b94acd8958b68498f02bc54da51ad29610837
Parthhiv <parthhivashley@gmail.com>
2023-11-20 17:12:22 +0530

    Finish Invoice Viewing

--- patient/view.php
+++ patient/view.php
@@ -23,6 +23,55 @@
                         include("sidenav.php");
                     ?>
                 </div>
+                
+                <div class="col-md-10">
+                    <h5 class="text-center my-2">View Invoice</h5>
+
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-3"></div>
+                            <div class="col-md-6">
+                                <?php
+                                    //check vid17 1:24:46
+                                    if (isset($_GET['id'])) {
+                                        $id = $_GET['id'];
+                                        $query = "SELECT * FROM income WHERE id='$id'";
+                                        $res = mysqli_query($connect,$query);
+                                        $row = mysqli_fetch_array($res);
+                                    }
+                                ?>
+                                
+                                <table class="table table-bordered">
+                                    <tr>
+                                        <td colspan="2" class="text-center">Invoice Details</td>
+                                    </tr>
+                                    
+                                    <tr>
+                                        <td>Doctor</td>
+                                        <td><?php echo $row['doctor']; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Patient</td>
+                                        <td><?php echo $row['patient']; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Date of Discharge</td>
+                                        <td><?php echo $row['date_discharge']; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Amount Paid</td>
+                                        <td><?php echo $row['amount_paid']; ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Description</td>
+                                        <td><?php echo $row['description']; ?></td>
+                                    </tr>
+                                </table>
+                            </div>
+                            <div class="col-md-3"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
