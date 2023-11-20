bcc16970b7756dd352c717bfc23e003b414d068d
Parthhiv <parthhivashley@gmail.com>
2023-11-20 16:14:07 +0530

    Add appointment view in doctor page

---
+++ doctor/appointment.php
@@ -0,0 +1,30 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Total Appointment</title>
+</head>
+<body>
+    <?php
+        include("../include/header.php");
+        include("../include/header.php");
+    ?>
+
+    <div class="container-fluid">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-2" style="margin-left: -30px;">
+                    <?php
+                    include("sidenav.php");
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
