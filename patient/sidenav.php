70305604a07baba470628882e8aa9458996524db
Jahnavi Anand <122967740+Jahnavi-Anand@users.noreply.github.com>
2023-11-23 16:58:49 +0530

    Updating sidenav connections

--- patient/sidenav.php
+++ patient/sidenav.php
@@ -14,11 +14,11 @@
         bg-info text-center text-white">Dashboard</a>
         <a href="profile.php" class="list-group-item list-group-item-action 
         bg-info text-center text-white">Profile</a>
-        <a href="#" class="list-group-item list-group-item-action 
+        <a href="appointment.php" class="list-group-item list-group-item-action 
         bg-info text-center text-white">Book Appointment</a>
         <a href="#" class="list-group-item list-group-item-action 
-        bg-info text-center text-white">Report</a>
-        <a href="#" class="list-group-item list-group-item-action 
+        bg-info text-center text-white">Report</a>  <!-- Report file needed --> 
+        <a href="invoice.php" class="list-group-item list-group-item-action 
         bg-info text-center text-white">Invoice</a>
 
     </div>
