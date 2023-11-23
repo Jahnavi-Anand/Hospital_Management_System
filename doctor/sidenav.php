68a78428268902afeb389a50b89223c1c73ee85f
Jahnavi Anand <122967740+Jahnavi-Anand@users.noreply.github.com>
2023-11-23 16:57:34 +0530

    Updating sidenav connections

--- doctor/sidenav.php
+++ doctor/sidenav.php
@@ -14,12 +14,12 @@
         bg-info text-center text-white">Dashboard</a>
         <a href="profile.php" class="list-group-item list-group-item-action 
         bg-info text-center text-white">Profile</a>
-        <a href="admin.php" class="list-group-item list-group-item-action 
+        <a href="./patient.php" class="list-group-item list-group-item-action 
         bg-info text-center text-white">Patient</a>
-        <a href="#" class="list-group-item list-group-item-action 
+        <a href="appointment.php" class="list-group-item list-group-item-action 
         bg-info text-center text-white">Appointment</a>
         <a href="#" class="list-group-item list-group-item-action 
-        bg-info text-center text-white">Report</a>
+        bg-info text-center text-white">Report</a> <!-- Report file needed-->
 
     </div>
     <!-- End Side Navigation -->
